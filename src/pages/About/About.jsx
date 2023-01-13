5bbf9a6975ee4857125fdd693f85e760849dc92d
Kelly Pham <phamkelly17@gmail.com>
2023-01-12 19:05:51 -0500

    changed about me description

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,8 +8,7 @@ const About = () => {
     <div id="aboutContainer">
       <Zoom>
       <p>Hi, my name is Kelly and I am a second year student studying Systems Design Engineering at the University of Waterloo.
-            What is Systems Design you ask? It's defined as "look[ing] at the big picture to find the connections that advance technology and innovation".
-            I'm specifically interested in applying software development to solve real world issues in different industries such as environment and healthcare.
+            What is Systems Design you ask? It's defined as "look[ing] at the big picture to find the connections that advance technology and innovation". I'm interested in full-stack development and more specifically, I'm passionate about building software applications that have an impact on sustainability or the healthcare industry. I'd love to connect and feel free to reach out at k27pham@uwaterloo.ca!
         </p>
       </Zoom>
     </div>
